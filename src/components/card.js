9e2120f35e0e40cedf513963ff215ef4b9f28052
boris <github@regmagik.com>
2025-11-06 17:03:22 -0500

    Update about page

---
+++ src/components/card.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+import { Card } from "reactstrap"
+
+function MyCard() {
+
+  return (
+    <Card>
+		📧 lenagabinskayardn@gmail.com
+		<br/>📞 (404) 884-2886
+    </Card>
+  )
+}
+
+export default MyCard
